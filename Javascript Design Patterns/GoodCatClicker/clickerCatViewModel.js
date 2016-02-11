b43f10d2c77a11b2c8e958ddb4a68ec01642af1c
Xyan Rose <xyanrose@gmail.com>
2016-02-10 21:03:37 -0500

    added backbone todo project

--- Javascript Design Patterns/GoodCatClicker/clickerCatViewModel.js
+++ Javascript Design Patterns/GoodCatClicker/clickerCatViewModel.js
@@ -1,22 +1,52 @@
+var initialCats = [
+	{
+		clickCount: 0,
+		name:'Tabby',
+		imgSrc: 'catClickerCat.jpg',
+		levels: {0:"DevilCat",10:"tabby",20:"garfield"},
+		nicknames: ['cat','tbone']
+	},
+	{
+		clickCount: 0,
+		name:'meow',
+		imgSrc: 'catClickerCat2.jpg',
+		levels: {0:"coolCat",10:"bubs",20:"mewowowowowowow"},
+		nicknames: ['mrown','mewmew']
+	}
+];
+var Cat = function(data){
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);//catClickerCat.jpg
+	this.levels = data.levels;//{0:"DevilCat",10:"tabby",20:"garfield"}
+	this.nicknames = ko.observableArray(this.nicknames);//['him','her']
+};
+
 var ViewModel = function (){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('DevilCat');
-	this.imgSrc = ko.observable('catClickerCat.jpg');
-	this.levels = {0:"DevilCat",10:"tabby",20:"garfield"};
+	var self = this;
+	this.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(kittyCat){
+		self.catList.push( new Cat(kittyCat));
+	});
+	self.currentCat = ko.observable( this.catList()[0]);
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
-		this.setLevel();
+		self.setLevel();
 	};
 
 	this.setLevel = function () {
-		console.log(Math.floor(this.clickCount()/10)*10);
-	 	this.name(this.levels[Math.floor(this.clickCount()/10)*10]);
+		if(self.currentCat().clickCount()<30){
+	 		this.currentCat().name(this.currentCat().levels[Math.floor(this.currentCat().clickCount()/10)*10]);
+		}
 	};
 
-	this.setName = function(name){
-		this.name(name);
+	this.swapCat = function(data){
+		console.log(data);
+		self.currentCat( data);
 	};
 }
 
 
 ko.applyBindings(new ViewModel());
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# Udacity
-Udacity Course Work
